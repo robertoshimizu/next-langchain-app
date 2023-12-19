8230e5b83a684966b60fdee3dbcb19e66bbff815
Roberto Shimizu <rshimizubr@gmail.com>
2023-12-18 21:02:27 -0300

    sign-in and sign-up pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,17 @@
 'use client';
+import { UserButton } from "@clerk/nextjs";
 
-import { useChat } from 'ai/react';
-
-export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
+export default function LandingPage() {
 
   return (
     <div className="mx-auto w-full max-w-md py-24 flex flex-col stretch">
-      {messages.length > 0
-        ? messages.map(m => (
-            <div key={m.id} className="whitespace-pre-wrap">
-              {m.role === 'user' ? 'User: ' : 'AI: '}
-              {m.content}
-            </div>
-          ))
-        : null}
+      <div className="whitespace-pre-wrap">
+        Welcome to the landing page!
+      </div>
+      <div className="h-screen">
+        <UserButton afterSignOutUrl="/"/>
+      </div>
 
-      <form onSubmit={handleSubmit}>
-        <input
-          className="fixed w-full max-w-md bottom-0 border border-gray-300 rounded mb-8 shadow-xl p-2"
-          value={input}
-          placeholder="Say something..."
-          onChange={handleInputChange}
-        />
-      </form>
     </div>
   );
 }
