5f4a5f9f2d2c4deb2c6b4ee8c777888d9492df62
Roberto Shimizu <rshimizubr@gmail.com>
2023-12-18 21:11:55 -0300

    set up props

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -3,7 +3,12 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
   <div className="mx-auto w-full max-w-md py-24 flex flex-col stretch">
-    <SignIn />
+    <SignIn
+      path="/sign-in"
+      signUpUrl="/sign-up"
+      afterSignInUrl="/chat"
+      afterSignUpUrl="/account"
+    />
   </div>
   )
 }
--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -3,7 +3,12 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
   <div className="mx-auto w-full max-w-md py-24 flex flex-col stretch">
-    <SignUp />
+    <SignUp
+      path="/sign-up"
+      signInUrl="/sign-in"
+      afterSignInUrl="/chat"
+      afterSignUpUrl="/account"
+    />
   </div>
   )
 }
