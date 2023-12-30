a9a9dec2f5e91bede4436b0de0f35e98e11e1287
Roberto Shimizu <rshimizubr@gmail.com>
2023-12-30 16:12:57 -0300

    protect routes

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -3,12 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
   <div className="mx-auto w-full max-w-md py-24 flex flex-col stretch">
-    <SignIn
-      path="/sign-in"
-      signUpUrl="/sign-up"
-      afterSignInUrl="/chat"
-      afterSignUpUrl="/account"
-    />
+    <SignIn/>
   </div>
   )
 }
--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -3,12 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
   <div className="mx-auto w-full max-w-md py-24 flex flex-col stretch">
-    <SignUp
-      path="/sign-up"
-      signInUrl="/sign-in"
-      afterSignInUrl="/chat"
-      afterSignUpUrl="/account"
-    />
+    <SignUp/>
   </div>
   )
 }
