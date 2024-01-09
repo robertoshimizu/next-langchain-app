887b09a2f172821633d268ec39aeb79d6850a171
Roberto Shimizu <rshimizubr@gmail.com>
2024-01-09 19:12:06 -0300

    upgrade langchain to use LCEL

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,31 +1,73 @@
-import { StreamingTextResponse, LangChainStream, Message } from 'ai';
-import { ChatOpenAI } from 'langchain/chat_models/openai';
-import { AIMessage, HumanMessage } from 'langchain/schema';
+import { NextRequest, NextResponse } from "next/server";
+import { Message as VercelChatMessage, StreamingTextResponse } from "ai";
 
-export const runtime = 'edge';
+import { ChatOpenAI } from "@langchain/openai";
+import { PromptTemplate } from "@langchain/core/prompts";
+import { HttpResponseOutputParser } from "langchain/output_parsers";
 
-export async function POST(req: Request) {
-  const { messages } = await req.json();
+export const runtime = "edge";
 
-  console.log(messages);
+const formatMessage = (message: VercelChatMessage) => {
+  return `${message.role}: ${message.content}`;
+};
 
-  const { stream, handlers } = LangChainStream();
+const TEMPLATE = `You are a pirate named Patchy. All responses must be extremely verbose and in pirate dialect.
 
-  const llm = new ChatOpenAI({
-    streaming: true,
-  });
+Current conversation:
+{chat_history}
 
-  llm
-    .call(
-      (messages as Message[]).map(m =>
-        m.role == 'user'
-          ? new HumanMessage(m.content)
-          : new AIMessage(m.content),
-      ),
-      {},
-      [handlers],
-    )
-    .catch(console.error);
+User: {input}
+AI:`;
 
-  return new StreamingTextResponse(stream);
+/**
+ * This handler initializes and calls a simple chain with a prompt,
+ * chat model, and output parser. See the docs for more information:
+ *
+ * https://js.langchain.com/docs/guides/expression_language/cookbook#prompttemplate--llm--outputparser
+ */
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+    const messages = body.messages ?? [];
+    const formattedPreviousMessages = messages.slice(0, -1).map(formatMessage);
+    const currentMessageContent = messages[messages.length - 1].content;
+    const prompt = PromptTemplate.fromTemplate(TEMPLATE);
+
+    /**
+     * You can also try e.g.:
+     *
+     * import { ChatAnthropic } from "langchain/chat_models/anthropic";
+     * const model = new ChatAnthropic({});
+     *
+     * See a full list of supported models at:
+     * https://js.langchain.com/docs/modules/model_io/models/
+     */
+    const model = new ChatOpenAI({
+      temperature: 0.8,
+      modelName: "gpt-3.5-turbo-1106",
+    });
+
+    /**
+     * Chat models stream message chunks rather than bytes, so this
+     * output parser handles serialization and byte-encoding.
+     */
+    const outputParser = new HttpResponseOutputParser();
+
+    /**
+     * Can also initialize as:
+     *
+     * import { RunnableSequence } from "langchain/schema/runnable";
+     * const chain = RunnableSequence.from([prompt, model, outputParser]);
+     */
+    const chain = prompt.pipe(model).pipe(outputParser);
+
+    const stream = await chain.stream({
+      chat_history: formattedPreviousMessages.join("\n"),
+      input: currentMessageContent,
+    });
+
+    return new StreamingTextResponse(stream);
+  } catch (e: any) {
+    return NextResponse.json({ error: e.message }, { status: 500 });
+  }
 }
