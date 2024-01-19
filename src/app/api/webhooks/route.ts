4b35eb8109d27884fda1ca6cb6bd5fb4499bf34c
Roberto Shimizu <rshimizubr@gmail.com>
2024-01-18 23:47:36 -0300

    remove export stripe

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -5,7 +5,7 @@ import { NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
 const secret: string = process.env.STRIPE_WEBHOOK_SECRET!
-export const stripe = new Stripe(String(process.env.STRIPE_SECRET_KEY), {
+const stripe = new Stripe(String(process.env.STRIPE_SECRET_KEY), {
     apiVersion: '2023-10-16',
 });
 
